349c1ab5e413249f85af0f506fa8bf5bdcdf9d9a
Nils Federle <nfederle@inovex.de>
2019-11-23 16:01:09 +0100

    Basic Clock with an Alarm Value

---
+++ simpleClock.py
@@ -0,0 +1,86 @@
+try: from Tkinter import *
+except ImportError: from tkinter import *
+import time
+
+root = Tk()
+root.title("Smart Clock v1")
+root.geometry("800x480")
+
+# create the main sections of the layout, 
+# and lay them out
+top = Frame(root, bg="black")
+top.pack(side=TOP, fill=BOTH, expand=True)
+
+bottom = Frame(root, bg="black")
+bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+alarm_settings = Frame(bottom, bg="black")
+alarm_settings.pack(fill = BOTH, expand=True, side=BOTTOM)
+
+alarm_settings_hrs = Frame(alarm_settings, bg="black")
+alarm_settings_hrs.pack(fill = BOTH, expand=True, side=LEFT)
+
+alarm_settings_min = Frame(alarm_settings, bg="black")
+alarm_settings_min.pack(fill = BOTH, expand=True, side=RIGHT)
+
+
+
+alarm_time_hrs = 11
+alarm_time_min = 00
+
+
+def tick():   
+    current_time = time.strftime('%H:%M:%S') 
+    if current_time != clock_label["text"]:
+        clock_label["text"] = current_time
+    clock_label.after(200, tick)
+
+def update_alarm():
+    global alarm_time_hrs
+    global alarm_time_min
+    
+    alarm_string= "{:02d}:{:02d}".format(alarm_time_hrs, alarm_time_min)
+    alarm_label["text"] = alarm_string
+    
+def inc_hrs():
+    global alarm_time_hrs
+    alarm_time_hrs =(alarm_time_hrs + 1)% 24
+    update_alarm()
+
+def dec_hrs():
+    global alarm_time_hrs
+    alarm_time_hrs = (alarm_time_hrs - 1)% 24
+    update_alarm()
+
+def inc_min():
+    global alarm_time_min
+    alarm_time_min =(alarm_time_min + 1)% 60
+    update_alarm()
+    
+def dec_min():
+    global alarm_time_min
+    alarm_time_min =(alarm_time_min - 1)% 60
+    update_alarm()
+    
+
+clock_label = Label(root, font=('times', 32, 'bold'), bg='black', fg="white")
+clock_label.pack(in_=top, fill=BOTH, expand=1)
+
+alarm_label = Label(root, font=('times', 32, 'bold'), bg='black', fg="white")
+alarm_label.pack (in_=bottom, fill= BOTH, expand=1)
+
+
+but_inc_min = Button(root, text="+ min",bg='black', fg="white", width=10, height=2, command=inc_min)
+but_dec_min = Button(root, text="- min",bg='black', fg="white", width=10, height=2, command=dec_min)
+but_inc_hrs = Button(root, text="+ hrs",bg='black', fg="white", width=10, height=2, command=inc_hrs)
+but_dec_hrs = Button(root, text="- hrs",bg='black', fg="white", width=10, height=2, command=dec_hrs)
+
+but_inc_min.pack(in_=alarm_settings_min)
+but_dec_min.pack(in_=alarm_settings_min)
+but_inc_hrs.pack(in_=alarm_settings_hrs)
+but_dec_hrs.pack(in_=alarm_settings_hrs)
+
+
+tick()
+update_alarm()
+root.mainloop()
