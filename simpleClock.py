840f8f64e77d9c0d7406660048933251ceb1738c
Nils Federle <nfederle@inovex.de>
2019-11-25 15:56:45 +0100

    added alarm toggle button and Alarm_Messagebox

--- simpleClock.py
+++ simpleClock.py
@@ -1,6 +1,7 @@
-try: from Tkinter import *
-except ImportError: from tkinter import *
-import time
+from tkinter import *
+from tkinter import messagebox
+
+import time, datetime
 
 root = Tk()
 root.title("Smart Clock v1")
@@ -27,20 +28,32 @@ alarm_settings_min.pack(fill = BOTH, expand=True, side=RIGHT)
 
 alarm_time_hrs = 11
 alarm_time_min = 00
+alarm_setting = False
 
-
-def tick():   
+def tick():
+    global alarm_time_hrs, alarm_time_min, alarm_setting
     current_time = time.strftime('%H:%M:%S') 
     if current_time != clock_label["text"]:
         clock_label["text"] = current_time
+    now = datetime.datetime.now()
+    if alarm_setting == True:
+        if ((alarm_time_hrs == now.hour) & (alarm_time_min == now.minute)):
+            messagebox.showinfo("ALARM!", "It's {:02d}:{:02d}".format(alarm_time_hrs, alarm_time_min))
+            alarm_setting = False
+            update_alarm()
+    
     clock_label.after(200, tick)
 
 def update_alarm():
     global alarm_time_hrs
     global alarm_time_min
     
-    alarm_string= "{:02d}:{:02d}".format(alarm_time_hrs, alarm_time_min)
+    alarm_string= "Alarm:\n{:02d}:{:02d}".format(alarm_time_hrs, alarm_time_min)
     alarm_label["text"] = alarm_string
+    if alarm_setting == True:
+        but_enable_alarm["text"] = "enabled"
+    else:
+        but_enable_alarm["text"] = "disabled"
     
 def inc_hrs():
     global alarm_time_hrs
@@ -61,12 +74,16 @@ def dec_min():
     global alarm_time_min
     alarm_time_min =(alarm_time_min - 1)% 60
     update_alarm()
-    
 
+def toggle_alarm():
+    global alarm_setting
+    alarm_setting = not alarm_setting
+    update_alarm()
+    
 clock_label = Label(root, font=('times', 32, 'bold'), bg='black', fg="white")
 clock_label.pack(in_=top, fill=BOTH, expand=1)
 
-alarm_label = Label(root, font=('times', 32, 'bold'), bg='black', fg="white")
+alarm_label = Label(root, font=('times', 24, 'bold'), bg='black', fg="white")
 alarm_label.pack (in_=bottom, fill= BOTH, expand=1)
 
 
@@ -80,6 +97,8 @@ but_dec_min.pack(in_=alarm_settings_min)
 but_inc_hrs.pack(in_=alarm_settings_hrs)
 but_dec_hrs.pack(in_=alarm_settings_hrs)
 
+but_enable_alarm = Button(root, text="disabled",bg='black', fg="white", width=20, height=2, command=toggle_alarm)
+but_enable_alarm.pack(in_=alarm_settings)
 
 tick()
 update_alarm()
